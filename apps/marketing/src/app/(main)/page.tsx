8380a16444c72ccc62181f7dad5051958769e281
e89wrhi <39992513+e89wrhi@users.noreply.github.com>
2026-04-07 04:08:52 +0300

    fix: marketing main page width

--- apps/marketing/src/app/(main)/page.tsx
+++ apps/marketing/src/app/(main)/page.tsx
@@ -1,13 +1,14 @@
+import ContentWidthWrapper from '@/components/layout/content-width-wrapper';
 import { Features } from '@/components/sections/features';
 import { Hero } from '@/components/sections/hero';
 import { TechStack } from '@/components/sections/techstack';
 
 export default function Home() {
   return (
-    <main className="max-w-6xl min-h-screen bg-white dark:bg-black">
+    <ContentWidthWrapper className="min-h-screen">
       <Hero />
       <Features />
       <TechStack />
-    </main>
+    </ContentWidthWrapper>
   );
 }
